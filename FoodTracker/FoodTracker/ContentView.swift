0fac14e7cd6d7c12df4a562482dffab7f5380cb5
zander <zander.ewell0759@stu.mtec.edu>
2023-03-15 23:36:07 -0600

    Updated saved project

--- FoodTracker/FoodTracker/ContentView.swift
+++ FoodTracker/FoodTracker/ContentView.swift
@@ -41,26 +41,26 @@ struct ContentView: View {
         
         NavigationView {
             ZStack {
-                
                 // - Creates a custom scrollable container
+
                 List {
+                    
                     // For each which grabs the dictionary data and loops through each of them and sorts them.
-                    ForEach(groupedFoodData.keys.sorted(), id: \.self) { key in
-                        // - creates a new section within the list and assigns a header to the section that displays the current key value. basically takes the values and displays them based on the expiration day name
+                    ForEach(["Expired", "Going bad soon", "Fresh"], id: \.self) { key in
                         Section(header: Text(key)) {
-                            // itterates over each of the items in the array that correspond to the 'key' or basically the name of the expiration and then executes the code within the curly brackets.
-                            ForEach(groupedFoodData[key]!, id: \.id) { food in
-                                // - This line of code creates a vertical stack view (VStack) that arranges two text views vertically. The first text view displays the name of the food, which is stored in the food property of the current item. The second text view displays the amount of the food that is left, which is stored in the amount property of the current item. The second text view is styled using a smaller font size and a gray color.
-                                VStack(alignment: .leading) {
-                                    Text(food.food)
-                                    Text("\(food.amount) left")
-                                        .font(.caption)
-                                        .foregroundColor(.gray)
+                            if let foods = groupedFoodData[key] {
+                                ForEach(foods, id: \.id) { food in
+                                    // - This line of code creates a vertical stack view (VStack) that arranges two text views vertically. The first text view displays the name of the food, which is stored in the food property of the current item. The second text view displays the amount of the food that is left, which is stored in the amount property of the current item. The second text view is styled using a smaller font size and a gray color.
+                                    VStack(alignment: .leading) {
+                                        Text(food.food)
+                                        Text("\(food.amount) left")
+                                            .font(.caption)
+                                            .foregroundColor(.gray)
+                                    }
                                 }
                             }
                         }
                     }
-                    
                 }
                 
                 .colorMultiply(.init("Color"))
